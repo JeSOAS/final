8a386993a3f971ec0eba4dbcf60a78ff5df12002
BuildTools <unconfigured@null.spigotmc.org>
2025-10-02 11:41:24 +0700

    Last commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "final",
-      cwd: "/home/codes/final",
+      name: "fin-customer",
+      cwd: "/home/codes/fin-customer",
       script: "pnpm",
       args: "start -- 3002",
       env: {
         NODE_ENV: "production",
         PORT: "3002"
       },
-      error_file: "/home/codes/final/pm2-error.log",
-      out_file: "/home/codes/final/pm2-out.log",
+      error_file: "/home/codes/fin-customer/pm2-error.log",
+      out_file: "/home/codes/fin-customer/pm2-out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss"
     }
   ]
